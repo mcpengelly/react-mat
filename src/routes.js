6d34248ec06849170ff539b3c86d08bc4061d9fc
mcpengelly <pengelly.mat@gmail.com>
2017-02-01 22:10:37 -0500

    initial

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import Layout from './components/Layout';
+import Index from './components/Index';
+import Athlete from './components/Athlete';
+import NotFoundPage from './components/NotFoundPage';
+
+
+const routes = (
+	<Route path="/" component={Layout}>
+		<IndexRoute component={IndexPage} />
+		<Route path="athlete/:id" component={AthletePage} />
+		<Route path="*" component={NotFoundPage} />
+	</Route>
+);
+
+export default routes;
